ab57388fb85d109ab44770edd500f1225d0cb816
Imanol Ramajo <imanol.ramajo@gmail.com>
2018-11-18 13:54:40 +0100

    Added NewsController

---
+++ src/DGamesBundle/Controller/NewsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DGamesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NewsController extends Controller
+{
+    public function showAction($id)
+    {
+        $new = $this->get('doctrine')->getManager()->getRepository("DGamesBundle:Noticia")->find($id);
+        if (!$new) {
+            throw $this->createNotFoundException('No se ha encontrado el juego.');
+        }
+        $opinions = $this->get('doctrine')->getManager()->getRepository("DGamesBundle:Opinion")->getOpinionsForGame($id);
+
+        return $this->render('DGamesBundle:News:show.html.twig', array('news' => $new, 'opinions' => $opinions));
+
+    }
+}
