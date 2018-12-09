9590ae0d19778e500d02ec9daf131ded841d4283
Imanol Ramajo <imanol.ramajo@gmail.com>
2018-12-09 17:19:30 +0100

    Animated tabs

--- web/js/main.js
+++ web/js/main.js
@@ -1,16 +1,12 @@
 
-
 jQuery(document).ready(function () {
-	// Give a different style to even comments
-	jQuery(".opinion:even").css("background", "#eee");
-	
-	// Change size of the game cover when hovering with mouse
-	var game_cover = jQuery(".game_cover")
-	var original_game_cover_width = game_cover.css("width");
-	game_cover.hover(function () {
-		jQuery(this).animate({width: "90%"});
+	// Smooth animation for the hovered tabs
+	var tabs = jQuery("#menu li a");
+	var original_width = tabs.css("width");
+	tabs.hover(function () {
+		jQuery(this).stop().animate({width: "100px"}, 300);
 	},
 	function () {
-		jQuery(this).animate({width: original_game_cover_width});
+		jQuery(this).stop().animate({width: original_width}, 100);		
 	});
 });
