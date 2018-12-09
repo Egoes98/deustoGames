89855e4baa85e6303e0c3341bc1f921757218101
Imanol Ramajo <imanol.ramajo@gmail.com>
2018-12-09 16:00:04 +0100

    Make the cover image bigger when hovering the mouse

--- web/js/main.js
+++ web/js/main.js
@@ -3,4 +3,14 @@
 jQuery(document).ready(function () {
 	// Give a different style to even comments
 	jQuery(".opinion:even").css("background", "#eee");
+	
+	// Change size of the game cover when hovering with mouse
+	var game_cover = jQuery(".game_cover")
+	var original_game_cover_width = game_cover.css("width");
+	game_cover.hover(function () {
+		jQuery(this).animate({width: "90%"});
+	},
+	function () {
+		jQuery(this).animate({width: original_game_cover_width});
+	});
 });
