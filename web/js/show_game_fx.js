866ec262a33511fdc9e4b0e4909817bffb8ace61
Imanol Ramajo del Castillo <imanol.ramajo@opendeusto.es>
2018-12-10 13:03:42 +0100

    Arreglado el escalado de la imagen

--- web/js/show_game_fx.js
+++ web/js/show_game_fx.js
@@ -1,14 +1,24 @@
+var game_cover;
+
+function game_cover_bigger() {
+	game_cover.stop().animate({width: "80%"});
+	game_cover.unbind("click");
+	game_cover.click(game_cover_smaller);
+}
+
+function game_cover_smaller() {
+	jQuery(this).stop().animate({width: "50%"});
+	game_cover.unbind("click");
+	game_cover.click(game_cover_bigger);
+}
+
 jQuery(document).ready(function () {
 	// Give a different style to even comments
 	jQuery(".opinion:even").css("background", "#eee");
 	
 	// Change size of the game cover when hovering with mouse
-	var game_cover = jQuery(".game_cover")
-	var original_game_cover_width = game_cover.css("width");
-	game_cover.hover(function () {
-		jQuery(this).animate({width: "80%"});
-	},
-	function () {
-		jQuery(this).animate({width: original_game_cover_width});
-	});
+	game_cover = jQuery(".game_cover")
+	original_game_cover_width = game_cover.css("width");
+	
+	game_cover.click(game_cover_bigger);
 });
